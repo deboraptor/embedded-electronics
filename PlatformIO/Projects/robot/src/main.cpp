405be19c1e88f886da50f46a042f9eb6631e814a
deboraptor <vdzdebora@gmail.com>
2025-09-27 21:03:02 +0200

    :recycle: remove unused includes to clean up main.cpp

--- PlatformIO/Projects/robot/src/main.cpp
+++ PlatformIO/Projects/robot/src/main.cpp
@@ -1,10 +1,6 @@
-#include <Adafruit_SPITFT_Macros.h>
-#include <Adafruit_GrayOLED.h>
 #include <Adafruit_GFX.h>
-#include <gfxfont.h>
-#include <Adafruit_SPITFT.h>
-#include <splash.h>
 #include <Adafruit_SSD1306.h>
+#include <Wire.h>
 
 // test d'un écran OLED 128x64 I2C
 // Objectif : synchroniser l'affichage des yeux sur 2 écrans OLED 
