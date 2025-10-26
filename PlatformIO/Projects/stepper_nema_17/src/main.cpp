a37db3c514ec841c67c30f444b9261c91e74259c
deboraptor <vdzdebora@gmail.com>
2025-10-26 21:27:51 +0100

    :recycle: remake code for stepper motor control using AccelStepper library that works after reversing wires

--- PlatformIO/Projects/stepper_nema_17/src/main.cpp
+++ PlatformIO/Projects/stepper_nema_17/src/main.cpp
@@ -1,21 +1,34 @@
-#include <Arduino.h>
+/*
+  doc : https://www.redohm.fr/download/arduino-et-moteurs-pas-a-pas-decouverte-de-la-bibliotheque-accelstepper/
 
-#define STEP_PIN 2
-#define DIR_PIN  5
-#define EN_PIN   8
+  Controlling two stepper with the AccelStepper library
+
+    by Dejan, https://howtomechatronics.com
+*/
+
+#include <AccelStepper.h>
+
+// Define the stepper motor and the pins that is connected to
+AccelStepper stepper1(1, 2, 5); // (Type of driver: with 2 pins, STEP, DIR)
 
 void setup() {
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(DIR_PIN,  OUTPUT);
-  pinMode(EN_PIN,   OUTPUT);
+  digitalWrite(8, HIGH); // Enable the motor driver (if applicable)
 
-  digitalWrite(EN_PIN, LOW);     // enable
-  digitalWrite(DIR_PIN, HIGH);   // sens (LOW pour inverser ?)
+  stepper1.setMaxSpeed(1000); // Set maximum speed value for the stepper
+  stepper1.setAcceleration(500); // Set acceleration value for the stepper
+  stepper1.setCurrentPosition(0); // Set the current position to 0 steps
 }
 
 void loop() {
-  digitalWrite(STEP_PIN, HIGH);
-  delay(2);                      // 2 ms up -> ~250 pas/s /2 = 125 steps/s
-  digitalWrite(STEP_PIN, LOW);
-  delay(2);
+
+  stepper1.moveTo(800); // Set desired move: 800 steps (in quater-step resolution that's one rotation)
+  stepper1.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
+
+  // Move back to position 0, using run() which is non-blocking - both motors will move at the same time
+  stepper1.moveTo(0);
+  while (stepper1.currentPosition() != 0) {
+    stepper1.run();  // Move or step the motor implementing accelerations and decelerations to achieve the target position. Non-blocking function
+    //
+    //
+  }
 }
