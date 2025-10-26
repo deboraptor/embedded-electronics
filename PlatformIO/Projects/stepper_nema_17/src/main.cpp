10b9d041e823c39ad0c757e1743b1f731c9387be
deboraptor <vdzdebora@gmail.com>
2025-10-26 21:50:09 +0100

    :art: update documentation and comments in main.cpp for clarity and consistency

--- PlatformIO/Projects/stepper_nema_17/src/main.cpp
+++ PlatformIO/Projects/stepper_nema_17/src/main.cpp
@@ -1,7 +1,7 @@
 /*
-  doc : https://www.redohm.fr/download/arduino-et-moteurs-pas-a-pas-decouverte-de-la-bibliotheque-accelstepper/
-
-  Controlling two stepper with the AccelStepper library
+  doc : https://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html
+  
+  Controlling two steppers with the AccelStepper library
 
     by Dejan, https://howtomechatronics.com
 */
@@ -9,14 +9,14 @@
 #include <AccelStepper.h>
 
 // Define the stepper motor and the pins that is connected to
-AccelStepper stepper1(1, 2, 5); // (Type of driver: with 2 pins, STEP, DIR)
+AccelStepper stepper1(1, 2, 5); // (toujours 1, stepPin, dirPin)
 
 void setup() {
   digitalWrite(8, HIGH); // Enable the motor driver (if applicable)
 
   stepper1.setMaxSpeed(1000); // Set maximum speed value for the stepper
   stepper1.setAcceleration(500); // Set acceleration value for the stepper
-  stepper1.setCurrentPosition(0); // Set the current position to 0 steps
+  stepper1.setCurrentPosition(0); // réinitialise la position actuelle à 0
 }
 
 void loop() {
@@ -24,11 +24,10 @@ void loop() {
   stepper1.moveTo(800); // Set desired move: 800 steps (in quater-step resolution that's one rotation)
   stepper1.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
 
-  // Move back to position 0, using run() which is non-blocking - both motors will move at the same time
-  stepper1.moveTo(0);
   while (stepper1.currentPosition() != 0) {
     stepper1.run();  // Move or step the motor implementing accelerations and decelerations to achieve the target position. Non-blocking function
-    //
-    //
   }
 }
+
+// stepper1.stop();
+// stepper1.run(); 
